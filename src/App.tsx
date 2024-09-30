ead0fec2d2f0f427ded118f90b0ddc475b7e8529
hleao <hbragaleao@gmail.com>
2024-09-29 23:49:13 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+import { Button } from "./components";
+
+export function App() {
+
+  return (
+    <div className='bg-slate-700 h-dvh'>
+      <h1>Home</h1>
+      <Button />
+    </div>
+  )
+}
+
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+
+export const Button = () => {
+  return (
+    <button className='bg-teal-500 text-white border-none rounded-sm px-4 py-2'>
+      button
+    </button>
+  );
+}
